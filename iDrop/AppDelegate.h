30fef32cdaa73b7cd0a5fea72ead87cfc06be162
Louk <Louk@naranbayars-macbook-pro.local>
2015-11-15 11:21:47 -0800

    Initial Commit

---
+++ iDrop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iDrop
+//
+//  Created by Naranbayar on 11/15/15.
+//  Copyright © 2015 Naranbayar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iDrop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iDrop
+//
+//  Created by Naranbayar on 11/15/15.
+//  Copyright © 2015 Naranbayar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
